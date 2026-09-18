84287cdc26f39f1365bb0ab313a6d11a9ad37c2e
Ash Joyce <ashrosejoyce@gmail.com>
2026-09-18 17:27:51 -0400

    Import environ properly

--- api/config/settings.py
+++ api/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,10 +75,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-import environ
-
-env = environ.Env()
 
 DATABASES = {
     'default': {
